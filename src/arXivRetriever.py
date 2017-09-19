d2d342f81098c017411c97d03499654346357570
sailiks <ssengu15@asu.edu>
2017-09-18 18:47:54 -0700

    Adding code for plotting graphs and logging progess

--- src/arXivRetriever.py
+++ src/arXivRetriever.py
@@ -1,30 +1,26 @@
 from graph_tool.all import *
+from pylab import *
 import arxivscraper
+import matplotlib
 import datetime
 import pandas
 import sys
 
 def plot_degrees(graph, date_from):
-	hist = vertex_hist(graph, deg="total")
-	#y = hist[0]
-	#err = sqrt(hist[0])
-	#err[err >= y] = y[err >= y] - 1e-2
-
-	figure(figsize=(6,4))
-	#errorbar(in_hist[1][:-1], in_hist[0], fmt="o", yerr=err,
-	#		label="in")
-	#gca().set_yscale("log")
-	#gca().set_xscale("log")
-	#gca().set_ylim(1e-1, 1e5)
-	#gca().set_xlim(0.8, 1e3)
-	#subplots_adjust(left=0.2, bottom=0.2)
-	xlabel("# of people")
-	ylabel("Co-author counts")
-	tight_layout()
-	savefig("{}.pdf".format(date_from))
-	savefig("{}.png".format(date_from)) 
+    hist = vertex_hist(graph, deg="total")
+    y = hist[0]
+    err = sqrt(hist[0])
+    err[err >= y] = y[err >= y] - 1e-2
+    errorbar(hist[1][:-1], hist[0], fmt="o",
+        label="total")
+    xlabel("Vertex (author) number")
+    ylabel("Co-authors")
+    tight_layout()
+    savefig("{}.pdf".format(date_from))
+    savefig("{}.png".format(date_from)) 
 
 if __name__ == '__main__':
+    
     if len(sys.argv) < 2:
         print("Please execute:\n python <codefile>.py <category_name> <start_date>")
         exit(1)
@@ -34,36 +30,46 @@ if __name__ == '__main__':
     date_from = sys.argv[2]
     date_until = str(datetime.date.today())
 
-    # Retrieve the records
-    scraper = arxivscraper.Scraper(category=category, date_from=date_from,date_until=date_until)
-    output = scraper.scrape()
-
-    # Store it in a panda dataframe
-    cols = ('id', 'title', 'categories', 'abstract', 'doi', 'created', 'updated', 'authors')
-    df = pandas.DataFrame(output, columns=cols)
-
-    # Create an adj list for authorship
-    co_auth_adj_list = {}
-    for author_list in df['authors']:
-        for u in author_list:
-            for v in author_list:
-                if not u == v:
-                    try:
-                        co_auth_adj_list[u].append(v)
-                    except:
-                        co_auth_adj_list[u] = []
-    
-    # Create co-authorship graph
+    graph_name = "graph_{}_{}.xml.gz".format(category, date_from)
     co_auth_graph = Graph(directed=False)
-    auth_vtx = {}
-    for a in co_auth_adj_list.keys():
-        v = co_auth_graph.add_vertex()
-        auth_vtx[a] = v
-    
-    for a in co_auth_adj_list.keys():
-        for b in co_auth_adj_list[a]:
-            co_auth_graph.add_edge(auth_vtx[a], auth_vtx[b])
+    try:
+        co_auth_graph.load(graph_name)
+    except:
+        print("[DEBUG] Graph data does not exist. Scraping ArXiv!")
+        # Retrieve the records
+        scraper = arxivscraper.Scraper(category=category, date_from=date_from,date_until=date_until)
+        output = scraper.scrape()
 
-    co_auth_graph.save("graph_{}.xml.gz".format(date_from))
-    graph_draw(co_auth_graph, sfdp_layour(g), output_size=(1000, 1000), vertex_color=[1,1,1,0], vertex_size=1, edge_pen_width=1.2, vcmap=matplotlib.cm.gist_heat_r, output="graph_{}.png".format(date_from) )
-    plot_degrees(co_auth_graph, date_from)
+        # Store it in a panda dataframe
+        cols = ('id', 'title', 'categories', 'abstract', 'doi', 'created', 'updated', 'authors')
+        df = pandas.DataFrame(output, columns=cols)
+
+        # Create an adj list for authorship
+        co_auth_adj_list = {}
+        for author_list in df['authors']:
+            for u in author_list:
+                for v in author_list:
+                    if not u == v:
+                        try:
+                            co_auth_adj_list[u].append(v)
+                        except:
+                            co_auth_adj_list[u] = []
+        
+        # Create co-authorship graph
+        auth_vtx = {}
+        for a in co_auth_adj_list.keys():
+            v = co_auth_graph.add_vertex()
+            auth_vtx[a] = v
+        
+        for a in co_auth_adj_list.keys():
+            for b in co_auth_adj_list[a]:
+                co_auth_graph.add_edge(auth_vtx[a], auth_vtx[b])
+
+        co_auth_graph.save(graph_name)
+        print("[DEBUG] Saved graph- {}".format(graph_name))
+    
+    try:
+        plot_degrees(co_auth_graph, date_from)
+        print("[SUCCESS] Successfully created the histogram!")
+    except:
+        print("[FAILED] Creating the histogram failed!")
